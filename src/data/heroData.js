e3028cc73e94c0fed5e5a6acefc4fe3c39160165
Amit Vishwakarma <vishamit8827@gmail.com>
2026-09-17 21:56:03 +0530

    Update hero content via admin dashboard

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -9,7 +9,7 @@ export const builtInHeroImageOptions = [
 ];
 
 export const defaultHero = {
-    "paragraph": "I help businesses build fast, user-friendly, and SEO-optimized websites using WordPress and Shopify. With 8+ years of hands-on experience, I turn ideas into high-performing digital platforms that drive results. hello",
+    "paragraph": "I help businesses build fast, user-friendly, and SEO-optimized websites using WordPress and Shopify. With 8+ years of hands-on experience, I turn ideas into high-performing digital platforms that drive results. hello Amit",
     "imgKey": "srDesigner",
     "imgUrl": ""
 };
