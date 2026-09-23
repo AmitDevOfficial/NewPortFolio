18d7492b9bf02f8a29f398b21c8cf8a48431c2a4
Amit Vishwakarma <vishamit8827@gmail.com>
2026-09-23 14:50:42 +0530

    Update hero content via admin dashboard

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -9,7 +9,7 @@ export const builtInHeroImageOptions = [
 ];
 
 export const defaultHero = {
-    "paragraph": "I build lightning-fast, SEO-ready WordPress and Shopify websites and scalable MERN stack apps. With 3year and 8 months of proven expertise, I turn ideas into powerful digital products driving measurable growth.",
+    "paragraph": "I build lightning-fast, SEO-ready MERN, Laravel, PHP, WordPress and Shopify websites and scalable MERN stack apps. With 3year and 8 months of proven expertise, I turn ideas into powerful digital products driving measurable growth.",
     "imgKey": "srDesigner",
     "imgUrl": "/uploads/hero-1789828190523-1.png"
 };
